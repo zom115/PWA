d5204e4eb5e46b12cb771d4be5e5ffa4e95d0073
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-01-06 19:38:20 +0900

    add timer

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,26 @@
+'use strict'
 const path = 'scripts/sw.js'
 navigator.serviceWorker.register(path)
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(path).then(registration => {
-    console.log('ServiceWorker registration successful with scope: ', registration.scope)
-  }).catch(err => {
-    console.log('ServiceWorker registration failed: ', err)
-  })
-}
-if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register(path).then(registration => {
-      // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope)
     }, err => {
-      // registration failed :(
       console.log('ServiceWorker registration failed: ', err)
     })
   })
 }
+
+let openTime = Date.now()
+document.getElementById('timeReset').addEventListener('click', () => openTime = Date.now())
+const countUp = () => {
+  const timerId = setTimeout(() => {
+    const elapsedTime = Date.now() - openTime
+    const mm = ('0' + Math.floor(elapsedTime / 6e4)).slice(-2)
+    const ss = ('0' + Math.floor(elapsedTime % 6e4 / 1e3)).slice(-2)
+    const ms = ('00' + elapsedTime % 1e3).slice(-3)
+    document.getElementById('currentTime').innerHTML = `${mm}:${ss}:${ms}`
+    countUp()
+  }, 16)
+}
+countUp()
