73bfbbf89443de925e4f5163017bc1fc14a79053
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-02-12 14:50:26 +0900

    add detail

--- scripts/main.js
+++ scripts/main.js
@@ -10,60 +10,38 @@ if ('serviceWorker' in navigator) {
     })
   })
 }
-const dbName = 'sampleDB'
+const dbName = 'indexedDB'
 const dbVersion = 1
-const storeName = 'sampleStore'
-const idName = 'sampleId'
+const storeName = ['site', 'market', 'statistics', 'setting']
+const idName = 'site'
 const materialName = 'crude'
-// const materialList = {
-//   tank: {name: 'tank'},
-//   engine: {name: 'generator engine'},
-//   rig: {name: 'rig'}
-// }
-// Object.keys(materialList).forEach(v => {
-//   console.log(materialList[v].name)
-// })
-{
-  const createE = (e, c, i = '', t = '', a) => {
-    const element = document.createElement(e)
-    element.className = c
-    element.id = i
-    element.textContent = t
-    if (a !== undefined) a.appendChild(element)
-    return element
-  }
-  const div = createE('div', 'cell', 'material')
-  const top = createE('div', 'container', '', '', div)
-  createE('span', '', 'name', '[name]', top)
-  createE('span', '', 'value', '[num]', top)
-  const middle = createE('div', 'container', '', '', div)
-  createE('button', '', 'button', '[cost: 0]', middle)
-  createE('span','','time', '[00:00]', middle)
-  const bottom = createE('div', 'container', '', '', div)
-  createE('progress', '', 'progressbar', '', bottom)
-  document.getElementById`column`.appendChild(div)
-}
+let firstBuildingArray = [
+  {name: 'Storage Tank'},
+  {name: 'Generator Engine'},
+  {name: 'Rig'}
+]
+let referenceLimit = 3
 const countLimit = 10 * 1e3
 const coolTimeLimit = 250
 let getTime = 0
 let takeFlag = false
 let db
 const openDb = () => {
-  console.log('openDb ...')
+  console.log('open DB ...')
   const request = indexedDB.open(dbName, dbVersion)
   request.onsuccess = () => {
     db = request.result
-    displayPubList()
-    console.log('openDb DONE')
+    displayColumn()
+    console.log('open DB done')
   }
   request.onupgradeneeded = e => {
     console.log('openDb.onupgradeneeded')
-    e.target.result.createObjectStore(storeName, {keyPath: idName})
+    e.target.result.createObjectStore(storeName[0], {keyPath: idName})
   }
   request.onerror = e => console.log('openDb:', e.target.errorCode)
 }
 const deleteDb = (bool = true) => {
-  console.log('deleteDb ...')
+  console.log('delete DB ...')
   const deleteRequest = indexedDB.deleteDatabase(dbName)
   deleteRequest.onsuccess = () => {
     console.log('delete DB success')
@@ -75,8 +53,25 @@ const deleteDb = (bool = true) => {
   }
   deleteRequest.onerror = () => console.log('delete DB error')
 }
+const dbInitialize = () => {
+  console.log('initialize ...')
+  document.getElementById`column`.textContent = null
+  const store = getObjectStore(storeName[0], 'readwrite')
+  firstBuildingArray.forEach((v, i) => {
+    v.detailFlag = false
+    v[idName] = i
+    const getFromId = store.get(v[idName])
+    getFromId.onsuccess = () => {
+      const req = store.put(firstBuildingArray[i])
+      req.onsuccess = () => {
+        if (i === 2) console.log('initialize DB success')
+      }
+      req.onerror = () => console.error('addPublication error', req.error)
+    }
+  })
+}
 const getObjectStore = (store_name, mode) => {
-  let tx = db.transaction(store_name, mode)
+  const tx = db.transaction(store_name, mode)
   return tx.objectStore(store_name)
 }
 const clearObjectStore = (store_name) => {
@@ -84,30 +79,102 @@ const clearObjectStore = (store_name) => {
   const store = getObjectStore(store_name, 'readwrite')
   const request = store.clear()
   request.onsuccess = () => {
-    displayPubList()
+    displayColumn()
     console.log('cleared')
   }
   request.onerror = e => {
     console.error('clearObjectStore:', e.target.errorCode)
   }
 }
-const displayPubList = store => {
-  if (typeof store === 'undefined') store = getObjectStore(storeName, 'readonly')
-  const getFromId = store.get(materialName)
+const generateColumn = (v, num) => {
+  const createE = (e, c, i = '', t = '', a) => {
+    const element = document.createElement(e)
+    element.className = c
+    element.id = i
+    element.textContent = t
+    if (a !== undefined) a.appendChild(element)
+    return element
+  }
+  const div = createE('div', 'cell', '', '', document.getElementById`column`)
+  const top = createE('div', 'container', '', '', div)
+  createE('progress', '', `progressbar-${num}`, '', top)
+  const middle = createE('div', 'container', '', '', div)
+  createE('span', '', `site-${num}`, num, middle)
+  createE('span', '', `name-${num}`, v.name, middle)
+  createE('span', '', `state-${num}`, '', middle)
+  const bottom = createE('div', 'container', '', '', div)
+  createE('span','',`time-${num}`, '', bottom)
+  const button = createE('button', '', `button-${num}`, '+', bottom)
+  const detail = createE('div', 'container', `detail-${num}`, '', div)
+  createE('span', '', '', v.name, detail)
+  const dummy1 = createE('div', 'container', '', '', div)
+  createE('span', '', '', v.name, dummy1)
+  const list = [detail, dummy1]
+  list.forEach(v => v.style.display = 'none')
+  button.addEventListener('click', () => {
+    button.textContent = button.textContent === '+' ? '-' : '+'
+    list.forEach(v => v.style.display = v.style.display === 'none' ? 'block' : 'none')
+  })
+}
+const displayColumn = () => {
+  const store = getObjectStore(storeName[0], 'readonly')
+  for (let i = 0; i < referenceLimit; i++) {
+    const getFromId = store.get(i)
+    getFromId.onsuccess = () => {
+      const r = getFromId.result
+      if (r === undefined) {
+        if (i === 0) {
+          dbInitialize()
+          displayColumn()
+        }
+      } else generateColumn(r, i)
+    }
+  }
+  // categoryList.forEach(v => {
+  //   const getFromId = store.get(v)
+  //   getFromId.onsuccess = () => {
+  //     const r = getFromId.result
+  //     if (v === categoryList[0]) {
+  //       if (r === undefined) {
+  //         dbInitialize()
+  //         displayColumn()
+  //       } else r.content.forEach((v, i) => generateColumn(v, i))
+  //     }
+      // const materialValue = (a !== undefined) ? a.value : 0
+      // document.getElementById`testText`.textContent = `${a[idName]}:`
+      // document.getElementById`testValue`.textContent = materialValue
+      // document.getElementById`testButton`.textContent = 'Cost: 10s'
+      // document.getElementById`progress`.value = 0
+      // document.getElementById`testTime`.textContent = ''
+  //   }
+  // })
+}
+const getObject = arg => {
+  const store = getObjectStore(storeName[0], 'readwrite')
+  const getFromId = store.get(arg)
   getFromId.onsuccess = () => {
-    const a = getFromId.result
-    const materialValue = (a !== undefined) ? a.value : 0
-    // document.getElementById`main`.textContent = `${materialName}: ${materialValue}`
-    document.getElementById`testText`.textContent = `${materialName}:`
-    document.getElementById`testValue`.textContent = materialValue
-    document.getElementById`testButton`.textContent = 'Cost: 10s'
-    document.getElementById`progress`.value = 0
-    document.getElementById`testTime`.textContent = ''
+    console.log(getFromId.result)
+    return getFromId.result
+  }
+}
+const switchDatailFlag = arg => {
+  const store = getObjectStore(storeName[0], 'readwrite')
+  const getFromId = store.get(arg)
+  getFromId.onsuccess = () => {
+    let r = getFromId.result
+    r.detailFlag = !r.detailFlag
+    const req = store.put(r)
+    // let req = store.put(data)
+    req.onsuccess = () => {
+      console.log('Insertion in DB successful')
+      // displayColumn()
+    }
+    req.onerror = () => console.error('addPublication error', req.error)
   }
 }
 const addPublication = arg => {
   console.log('add ...')
-  const store = getObjectStore(storeName, 'readwrite')
+  const store = getObjectStore(storeName[0], 'readwrite')
   const getFromId = store.get(arg)
   getFromId.onsuccess = () => {
     const a = getFromId.result
@@ -116,23 +183,12 @@ const addPublication = arg => {
     let req = store.put(data)
     req.onsuccess = () => {
       console.log('Insertion in DB successful')
-      displayPubList()
+      displayColumn()
     }
     req.onerror = () => {
       console.error('addPublication error', req.error)
     }
   }
-}
-const addEventListeners = msg => {
-  console.log('addEventListeners')
-  // document.getElementById`add`.addEventListener('click', () => addPublication(materialName))
-  document.getElementById`clear`.addEventListener('click', () => clearObjectStore(storeName))
-  document.getElementById`deleteDb`.addEventListener('click', () => deleteDb())
-  document.getElementById`deleteDev`.addEventListener('click', () => deleteDb(false))
-  document.getElementById`testButton`.addEventListener('click', e => {
-    e.target.disabled = true
-    getTime = Date.now()
-  })
 }
 const materialProcess = () => {
   if (getTime !== 0) {
@@ -166,8 +222,19 @@ const formatTime = argTime => {
   if (0 < mm) time = mm + ':' + time
   return time
 }
+const addEventListeners = msg => {
+  console.log('addEventListeners')
+  // document.getElementById`add`.addEventListener('click', () => addPublication(materialName))
+  // document.getElementById`clear`.addEventListener('click', () => clearObjectStore(storeName))
+  document.getElementById`deleteDb`.addEventListener('click', () => deleteDb())
+  document.getElementById`deleteDev`.addEventListener('click', () => deleteDb(false))
+  // document.getElementById`testButton`.addEventListener('click', e => {
+  //   e.target.disabled = true
+  //   getTime = Date.now()
+  // })
+}
 const main = () => {
-  materialProcess()
+  // materialProcess()
   document.getElementById`conectedTime`.textContent = formatTime(Date.now() - openTime)
   window.requestAnimationFrame(main)
 }
